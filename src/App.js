d7cd79626eb482603911d63479d33c4d2fe10ea3
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-07-07 11:10:19 +0530

    yes me

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ function App() {
       {
         status?<h1>this is going to hide !!</h1>:null
       }
-      <button onClick={()=>setStatus(false)}>Hide</button>
-      <button onClick={()=>setStatus(true)}>Show</button>
+      {/* <button onClick={()=>setStatus(false)}>Hide</button>
+      <button onClick={()=>setStatus(true)}>Show</button> */}
+      <button onClick={()=>setStatus(!status)}>Toggle</button>
     </div>
   )
 }
