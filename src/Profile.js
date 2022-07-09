bfd86ae1bb332930e35f3978e0be85e711c6228f
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-07-09 10:59:51 +0530

    yes me

--- src/Profile.js
+++ src/Profile.js
@@ -1,7 +1,19 @@
-export default function Profile(props){
-    return(
-        <div className="App">
-            <h1>This is profile component</h1>
-        </div>
-    )
+import { useState } from "react";
+export default function Profile(props) {
+  const [loggedIn, setStatus] = useState(false);
+  // * now learning ternary operators
+  // ! this if else statements can be replace with ternary operators
+  //   if (loggedIn) {
+  //     return (
+  //       <div className="App">
+  //         <h1>Welcome Monu</h1>
+  //       </div>
+  //     );
+  //   } else {
+  //     return (
+  //       <div className="App">
+  //         <h1>Welcome guest</h1>
+  //       </div>
+  //     );
+  //   }
 }
