e4ee8916f5c02b383ddcdca0f632a7d16cfe1e51
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-01 19:53:49 +0530

    yes me

--- src/App.js
+++ src/App.js
@@ -1,43 +1,110 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import { Table } from "react-bootstrap";
-// # API calling in react js
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+// # POST API in react js using form
 function App() {
-  let [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://amazoncloneserver.herokuapp.com/products").then((result) => {
-      result.json().then((resp) => {
-        setData(resp);
-      });
-    });
-  }, []);
-  console.log(data);
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  function saveData(e) {
+    e.preventDefault();
+    let data = {name, email, password};
+    fetch("https://testapimdkaif.herokuapp.com/Data/Add",{
+      method: "POST",
+      headers: {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json'
+      },
+      body:JSON.stringify(data)
+    }).then((result) => {
+      console.log("result ", result);
+    })
+  }
   return (
-    <div className="App">
-      <h1>This is for API calling</h1>
-      <Table striped hover variant="dark">
-        <thead>
-          <tr>
-            <th>S.No</th>
-            <th>Name</th>
-            <th>Brand</th>
-            <th>Cost (in Rupees)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, i) => (
-            <tr key={i}>
-              <td>{i}</td>
-              <td>{item.name}</td>
-              <td>{item.brand}</td>
-              <td>{item.cost}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+    <div className="container">
+      <h1 className="App">Post API in react js</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your identity
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={saveData}>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
+// # API calling in react js GET API
+// import React, { useState, useEffect } from "react";
+// import { Table } from "react-bootstrap";
+// function App() {
+//   let [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://amazoncloneserver.herokuapp.com/products").then((result) => {
+//       result.json().then((resp) => {
+//         setData(resp);
+//       });
+//     });
+//   }, []);
+//   console.log(data);
+//   return (
+//     <div className="App">
+//       <h1>This is for API calling</h1>
+//       <Table striped hover variant="dark">
+//         <thead>
+//           <tr>
+//             <th>S.No</th>
+//             <th>Name</th>
+//             <th>Brand</th>
+//             <th>Cost (in Rupees)</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {data.map((item, i) => (
+//             <tr key={i}>
+//               <td>{i+1}</td>
+//               <td>{item.name}</td>
+//               <td>{item.brand}</td>
+//               <td>{item.cost}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </Table>
+//     </div>
+//   );
+// }
 
 // # React Router (Dynamic also)
 // ! In react v6 For route you have to put all the route in the Routes tag
