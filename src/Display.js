1c7e9183f0fd83cf128a73e693cc7cc4560a1747
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-03 07:42:41 +0530

    yes me

--- src/Display.js
+++ src/Display.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useRef } from "react";
 export default function Display(props) {
   const lastVal = useRef();
+  const lastValLast = useRef();
   useEffect(() => {
+    lastValLast.current = lastVal.current;
     lastVal.current = props.count;
     // # It will pick the value of count from the the dom
   });
-  const previousVal = lastVal.current;
+  // const previousVal = lastVal.current;
   return (
     <div className="container">
       <h1>This the current value : {props.count}</h1>
-      <h1>This the Previous value : {previousVal}</h1>
+      <h1>This the Previous value : {lastVal.current}</h1>
+      <h1>This the Previous value : {lastValLast.current}</h1>
     </div>
   );
 }
