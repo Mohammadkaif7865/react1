53df97e2db01fe0f78fd0b949e004b27a7edf29b
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-08-05 11:11:54 +0530

    yes me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,79 @@
 import "./App.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 function App() {
-  return(
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [role, setRole] = useState("");
+  function login(e) {
+   e.preventDefault();
+   let data = {name,email,password,role};
+   fetch("https://authmdkaif.herokuapp.com/api/auth/register", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((result) => {
+    console.log("result ", result);
+  });
+  }
+  return (
     <div className="container">
       <h1> This is the authentication Page</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your identity
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicRole">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={login}>
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
 // # React common context
 // import React, { useState } from "react";
